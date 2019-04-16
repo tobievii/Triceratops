8f0c6bd36c1330273299a3a54bd810a2c38ad4d3
Unknown <tobie.alberts@gmail.com>
2019-04-16 14:16:25 +0200

    Added TP-Link SmartPlugs

---
+++ examples/SmartPlugs/tplink/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='pyHS100',
+      version='0.3.5',
+      description='Interface for TPLink HS1xx plugs, HS2xx wall switches & LB1xx bulbs',
+      url='https://github.com/GadgetReactor/pyHS100',
+      author='Sean Seah (GadgetReactor)',
+      author_email='sean@gadgetreactor.com',
+      license='GPLv3',
+      packages=['pyHS100'],
+      install_requires=['click', 'click-datetime', 'typing'],
+      python_requires='>=3.4',
+      entry_points={
+            'console_scripts': [
+                  'pyhs100=pyHS100.cli:cli',
+            ],
+      },
+      zip_safe=False)
